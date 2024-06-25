320a429f386692b5ef0e9e62a7e9fd69c76e8844
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-06-25 11:30:50 +0530

    feat: added dummy questions data

---
+++ src/questions.jsx
@@ -0,0 +1,60 @@
+export const customQuestions = [
+  {
+    Statement:
+      "Our church's ministry strategy is firm but flexible to accommodate unexpected game changers such as AI.",
+    num: 1,
+  },
+  {
+    Statement:
+      "Our leadership team knows of the potential risks and rewardsof leveraging AI.",
+    num: 2,
+  },
+  {
+    Statement:
+      "Our leadership team understands the value of tapping experts to educate our staff about AI.",
+    num: 3,
+  },
+  {
+    Statement:
+      "Our leadership team understands the value of tapping experts to educate our staff about AI.",
+    num: 4,
+  },
+  {
+    Statement: "Our s to educate our staff about AI.",
+    num: 5,
+  },
+  {
+    Statement:
+      "Our leadership team understands the value of e our staff about AI.",
+    num: 6,
+  },
+  {
+    Statement:
+      "Our church's ministry strategy is firm but flexible to accommodate unexpected game changers such as AI.",
+    num: 7,
+  },
+  {
+    Statement:
+      "Our leadership team knows of the potential risks and rewardsof leveraging AI.",
+    num: 8,
+  },
+  {
+    Statement:
+      "Our leadership team understands the value of tapping experts to educate our staff about AI.",
+    num: 9,
+  },
+  {
+    Statement:
+      "Our leadership team understands the value of tapping experts to educate our staff about AI.",
+    num: 10,
+  },
+  {
+    Statement: "Our s to educate our staff about AI.",
+    num: 11,
+  },
+  {
+    Statement:
+      "Our leadership team understands the value of e our staff about AI.",
+    num: 12,
+  },
+];
