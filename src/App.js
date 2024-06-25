ea14e8264882d0721848978987477b06be058d3f
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-06-25 19:26:41 +0530

    added slider functionality

--- src/App.js
+++ src/App.js
@@ -1,37 +1,135 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { customQuestions } from "./questions";
 
 export default function App() {
-    const [currQues, setCurrQues] = useState(1);
-    const [isSmallScreen, setSmallScreen] = useState(
-      window.matchMedia("(max-width: 768px)").matches
-    );
+  
+  const [currQues, setCurrQues] = useState(1);
+  const [val, setVal] = useState(false);
+  const [currentQuestionNum, setCurrentQuestionNum] = useState(1);
+  const [progressBars, setProgressBars] = useState([
+    { name: "STRATEGY", score: 400 / customQuestions.length, key: 1 },
+    { name: "UNDERSTANDING", score: 0, key: 2 },
+    { name: "APPLICATION", score: 0, key: 3 },
+    { name: "DIRECTION", score: 0, key: 4 },
+  ]);
 
-    const navigate = useNavigate();
 
-    const handleNavigationClick = () => {
-      navigate("/");
+  const [isSmallScreen, setSmallScreen] = useState(
+    window.matchMedia("(max-width: 768px)").matches
+  );
+
+  useEffect(() => {
+    const handleScreenSizeChange = (e) => setSmallScreen(e.matches);
+    const mediaQueryList = window.matchMedia("(max-width: 768px)");
+
+    mediaQueryList.addEventListener("change", handleScreenSizeChange);
+
+    return () => {
+      mediaQueryList.removeEventListener("change", handleScreenSizeChange);
     };
+  }, []);
 
-    useEffect(() => {
-      const handleScreenSizeChange = (e) => setSmallScreen(e.matches);
-      const mediaQueryList = window.matchMedia("(max-width: 768px)");
 
-      mediaQueryList.addEventListener("change", handleScreenSizeChange);
+  const [sliderValue, setSliderValue] = useState("0");
+  const [responseRecords, setResponseRecords] = useState([]);
 
-      return () => {
-        mediaQueryList.removeEventListener("change", handleScreenSizeChange);
-      };
-    }, []);
+  function handleQuestionNavigation(direction) {
+    let matchingRecord = checkMatchingRecord(currentQuestionNum);
 
-    const [currentQuestionNum, setCurrentQuestionNum] = useState(1);
-    const [progressBars, setProgressBars] = useState([
-      { name: "STRATEGY", score: 400 / customQuestions.length, key: 1 },
-      { name: "UNDERSTANDING", score: 0, key: 2 },
-      { name: "APPLICATION", score: 0, key: 3 },
-      { name: "DIRECTION", score: 0, key: 4 },
-    ]);
+    if (
+      direction === "forward" &&
+      currentQuestionNum !== 12 &&
+      matchingRecord.length > 0
+    ) {
+      let nextQuestionRecord = checkMatchingRecord(currentQuestionNum + 1);
+
+      if (nextQuestionRecord.length > 0) {
+        setSliderValue(nextQuestionRecord[0].sliderValue);
+      } else {
+        setSliderValue("0");
+      }
+
+      handleProgressBarUpdate("Increase");
+      setCurrentQuestionNum((prev) => prev + 1);
+    } else if (direction === "back" && currentQuestionNum !== 1) {
+      let previousQuestionRecord = checkMatchingRecord(currentQuestionNum - 1);
+      setSliderValue(previousQuestionRecord[0].sliderValue);
+      handleProgressBarUpdate("Decrease");
+      setCurrentQuestionNum((prev) => prev - 1);
+    } else if (direction === "back" && currentQuestionNum === 1) {
+    }
+  }
+
+  function handleSliderChange(event) {
+    if (responseRecords.length === 0) {
+      setResponseRecords([
+        { ques: currentQuestionNum, sliderValue: event.target.value },
+      ]);
+    } else {
+      let matchingRecord = checkMatchingRecord(currentQuestionNum);
+
+      if (matchingRecord.length > 0) {
+        setResponseRecords((prev) =>
+          prev.map((element) =>
+            element.ques === currentQuestionNum
+              ? { ...element, sliderValue: event.target.value }
+              : { ...element }
+          )
+        );
+      } else {
+        setResponseRecords((prev) => [
+          ...prev,
+          { ques: currentQuestionNum, sliderValue: event.target.value },
+        ]);
+      }
+    }
+
+    setSliderValue(event.target.value);
+    setTimeout(() => {
+      setSliderValue("0");
+    }, 250);
+
+    if (currentQuestionNum !== 12) {
+      handleProgressBarUpdate("Increase");
+      setCurrentQuestionNum((prev) => prev + 1);
+    }
+  }
+
+  function checkMatchingRecord(num) {
+    return responseRecords.filter((element) => num === element.ques);
+  }
+
+  let currentQuestionStatement = customQuestions.filter(
+    (element) => currentQuestionNum === element.num
+  );
 
   
+
+  useEffect(() => {
+    if (val) {
+      let quesNo = Math.ceil(currQues / 3);
+      console.log(quesNo, "quesNo", currQues, progressBars);
+
+      setProgressBars((prev) =>
+        prev.map((element) =>
+          element.key <= quesNo
+            ? {
+                ...element,
+                score: element.score + 400 / customQuestions.length,
+              }
+            : { ...element }
+        )
+      );
+    } else {
+      setVal(true);
+    }
+  }, [currQues]);
+
+  function handleProgressBarUpdate(state) {
+    console.log("currQues", currQues);
+    setCurrQues((prev) => prev + 1);
+  }
   return (
     <section className="CustomQuestionnaire">
       <div className="QuestionnaireContainer">
@@ -71,8 +169,50 @@ export default function App() {
         <p className="QuestionStatement">
           {currentQuestionStatement[0].Statement}
         </p>
-       
-      
+        <div className="SliderWrapper">
+          <input
+            id="Slider"
+            step={25}
+            value={sliderValue}
+            type="range"
+            list="sliderOptions"
+            onChange={handleSliderChange}
+          />
+          <datalist id="sliderOptions">
+            <option value="0" label="Strongly Disagree"></option>
+            <option value="25" label="Disagree"></option>
+            <option value="50" label="Neutral"></option>
+            <option value="75" label="Agree"></option>
+            <option value="100" label="Strongly Agree"></option>
+          </datalist>
+        </div>
+        <div className="NavigationButtons">
+          {currentQuestionNum != 1 ? (
+            <div
+              onClick={() => {
+                handleQuestionNavigation("back");
+              }}
+              className="ButtonWrapper"
+            >
+              <p>PREV</p>
+            </div>
+          ) : (
+            <div></div>
+          )}
+
+          {currentQuestionNum > 0 && currentQuestionNum !== 12 && (
+            <div
+              onClick={() => {
+                handleQuestionNavigation("forward");
+              }}
+              className="ButtonWrapper"
+            >
+              <p style={{ borderBottom: "1px solid rgb(110, 12, 249)" }}>
+                NEXT
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
